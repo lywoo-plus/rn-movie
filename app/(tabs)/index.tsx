728e3798c85cf6585f5f6e3bb4239ef8bebab04f
lywoo-plus <sreng.lywoo@gmail.com>
2026-01-26 23:42:45 +0700

    feat: add bottom navigation bar

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function index() {
+  return (
+    <View className="flex items-center justify-center h-full w-full">
+      <Text className="text-2xl">Home screen</Text>
+    </View>
+  );
+}
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const index = () => {
+export default function SearchScreen() {
   return (
     <View>
-      <Text>index</Text>
+      <Text>SearchScreen</Text>
     </View>
   );
-};
-
-export default index;
+}
